0330375ff0286c81577107bca976fc0b9cab9552
Max Jeffrey <74653415+itsmaxjeffrey@users.noreply.github.com>
2025-03-14 00:02:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Group 39</h1>
         <Image
           className={styles.logo}
           src="/next.svg"
