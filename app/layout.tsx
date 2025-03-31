934369594cc69346b2fd05083bd59eda7dce6e19
ojg9 <oliverjavier.groener@uzh.ch>
2025-03-31 16:18:07 +0200

    Formating and linting done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ConfigProvider} from "antd";
+import { ConfigProvider } from "antd";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "@/styles/globals.css";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
